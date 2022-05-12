cee46157c2ee1bcdd3a5080039f24f64a4f01d11
GangsuKim <gangsu1813@gmail.com>
2022-05-13 03:53:17 +0900

    Add stock.js

---
+++ stock.js
@@ -0,0 +1,12 @@
+// fetch("https://openapivts.koreainvestment.com:29443/oauth2/tokenP", {
+//     method: "POST",
+//     mode: "no-cors",
+//     headers: {
+//         "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({
+//         "grant_type": "client_credentials",
+//         "appkey": "PSbLrnsPeogvrYLUivntuyNQcfDwpo7Yrijcv",
+//         "appSecrect": "r+tgaZ9ZuCEnMuE9Ow1iQ749SMptXORvONK6CR1l+swcZNSY/annJDw8PfGMUAmdXzejIzWamUkokyuBqkEUqv5oGxtOxWO1ZVsE0FfewKgLlB3Wk7mgytKp0oqZj0hCZQ6MML2Jq5qratcnEBqMdqNDuoGTY4fS6oMrr7670mNWW/LUVUk=v",
+//     })
+// }).then((res) => alert(res.access_token));
