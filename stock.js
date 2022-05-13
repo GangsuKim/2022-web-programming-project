ad558d4cc359cac52c022d8268b0c8dc6c55dbf5
GangsuKim <gangsu1813@gmail.com>
2022-05-13 21:37:42 +0900

    Working on price

--- stock.js
+++ stock.js
@@ -1,12 +1,56 @@
-// fetch("https://openapivts.koreainvestment.com:29443/oauth2/tokenP", {
-//     method: "POST",
-//     mode: "no-cors",
-//     headers: {
-//         "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({
-//         "grant_type": "client_credentials",
-//         "appkey": "PSbLrnsPeogvrYLUivntuyNQcfDwpo7Yrijcv",
-//         "appSecrect": "r+tgaZ9ZuCEnMuE9Ow1iQ749SMptXORvONK6CR1l+swcZNSY/annJDw8PfGMUAmdXzejIzWamUkokyuBqkEUqv5oGxtOxWO1ZVsE0FfewKgLlB3Wk7mgytKp0oqZj0hCZQ6MML2Jq5qratcnEBqMdqNDuoGTY4fS6oMrr7670mNWW/LUVUk=v",
-//     })
-// }).then((res) => alert(res.access_token));
+var USDKRW, GOOGL, META, GOOGL_DONE, META_DONE;
+const GOOGL_CNT = 2, META_CNT = 2;
+
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Host': 'yh-finance.p.rapidapi.com',
+        'X-RapidAPI-Key': '6e1e98fe82msh9b0b06f7a98c2d2p1c8126jsn776f8ccf5839'
+    }
+};
+
+const texts = document.querySelectorAll('#stockPrice');
+const totalPrice = document.querySelector('#totlaPrice');
+
+fetch('https://api.currencyfreaks.com/latest?apikey=239c9b6b6e9645ec91dc09654fa9f24d').then(res => res.json()).then(res => {
+    USDKRW = res.rates.KRW
+
+    fetch('https://yh-finance.p.rapidapi.com/stock/v3/get-chart?interval=1m&symbol=GOOGL&range=1d&region=HK&includePrePost=false&useYfid=true&includeAdjustedClose=true&events=capitalGain%2Cdiv%2Csplit', options)
+        .then(response => response.json())
+        .then(response => {
+            GOOGL = parseInt(response['chart']['result'][0]['meta']['regularMarketPrice'] * USDKRW);
+            texts[0].innerText = (GOOGL * GOOGL_CNT).toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",") + '원';
+            GOOGL_DONE = true;
+            setUpPrice();
+        });
+
+    fetch('https://yh-finance.p.rapidapi.com/stock/v3/get-chart?interval=1m&symbol=FB&range=1d&region=HK&includePrePost=false&useYfid=true&includeAdjustedClose=true&events=capitalGain%2Cdiv%2Csplit', options)
+        .then(response => response.json())
+        .then(response => {
+            META = parseInt(response['chart']['result'][0]['meta']['regularMarketPrice'] * USDKRW);
+            alert(addComma(META + ''));
+            texts[1].innerText = (META * META_CNT).toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",") + '원';
+            META_DONE = true;
+            setUpPrice();
+        });
+});
+
+function setUpPrice() {
+    if(GOOGL_DONE && META_DONE) {
+        var price = (parseInt(GOOGL) * 2) + (parseInt(META) * 2) + '';
+        totalPrice.innerText = price.replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",") + '원';
+    }
+}
+
+function addComma(plainPriceText) {
+    var commatext, c = 0;
+    for (var i = plainPriceText.length - 1; i >= 0; i--) {
+        c++;
+        if(c == 3) {
+            commatext = ',' + plainPriceText[i] + commatext;
+        } else {
+            commatext = plainPriceText[i] + commatext;
+        }
+    }
+    return commatext;
+}
