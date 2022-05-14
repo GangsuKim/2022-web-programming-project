3f6f0bff3e27bcc8dc4a560b1948199702a40f7d
GangsuKim <gangsu1813@gmail.com>
2022-05-15 00:32:37 +0900

    Add MFB

--- js/stock.js
+++ js/stock.js
@@ -90,44 +90,6 @@ function setUpPrice() {
     }
 }
 
-function addComma(plainPriceText) {
-    var commatext = '',
-        c = 0;
-    plainPriceText += '';
-    for (var i = plainPriceText.length - 1; i >= 0; i--) {
-        c++;
-        if (c == 3 && i != 0) {
-            commatext = ',' + plainPriceText[i] + commatext;
-            c = 0;
-        } else {
-            commatext = plainPriceText[i] + commatext;
-        }
-    }
-    return commatext
-}
-
-function showTime() {
-    let today = new Date();
-    const timeA = document.getElementById('timeA');
-
-    let year = today.getFullYear();
-    let month = toFullword(today.getMonth() + 1);
-    let date = toFullword(today.getDate());
-    let hour = toFullword(today.getHours());
-    let min = toFullword(today.getMinutes());
-    let sec = toFullword(today.getSeconds());
-
-    var timeStamp = year + '-' + month + '-' + date + ' ' + hour + ':' + min + ':' + sec;
-    timeA.innerText = timeStamp;
-}
-
-function toFullword(string) {
-    if (string < 10) {
-        string = '0' + string;
-    }
-    return string
-}
-
 function googlePrice() {
     const googlePriceSpans = document.getElementsByClassName('googlePrice');
 
@@ -139,12 +101,6 @@ function googlePrice() {
             googlePriceSpans[i].innerText = addComma(priceTemp) + '원';
         }
     }
-}
-
-function calcPM(now,bought) {
-    var percent = (now / bought) * 1000;
-    var PM = Math.round((percent - 1000) / 10);
-    return PM
 }
 
 document.getElementById('boardBefore').addEventListener('click', handleBoardBefore);
