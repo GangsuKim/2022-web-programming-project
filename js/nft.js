a6858cbf6db3b51962ab4e8acdebee497a5df5ac
GangsuKim <gangsu1813@gmail.com>
2022-06-12 02:12:50 +0900

    Working on NFTs

--- js/nft.js
+++ js/nft.js
@@ -35,12 +35,119 @@ var NFTs = [
         imgName: 'SF_130',
         number: 237783,
         collection: 'sheepFarm'
+    }), Object({
+        type: 'Rare',
+        bought: 280,
+        now: 50,
+        imgName: 'MFB_6741',
+        number: 6741,
+        collection: 'My Fat Babiz'
+    }), Object({
+        type: 'Uncommon',
+        bought: 250,
+        now: 50,
+        imgName: 'MFB_7211',
+        number: 7211,
+        collection: 'My Fat Babiz'
+    }), Object({
+        type: 'Uncommon',
+        bought: 140,
+        now: 200,
+        imgName: 'DKR_8394',
+        number: 8394,
+        collection: 'DKR'
+    }), Object({
+        type: 'Mongz',
+        bought: 75,
+        now: 35,
+        imgName: 'BM_1649',
+        number: 1649,
+        collection: 'Baby Mongz'
+    }), Object({
+        type: 'Mongz',
+        bought: 75,
+        now: 35,
+        imgName: 'BM_8851',
+        number: 8851,
+        collection: 'Baby Mongz'
     })
 ];
 
-$(document).ready(function() {
-    $('.NFTBarTop').each(function(i,collect) {
-        $(collect).children('.collection').text(NFTs[i].collection);
-        $(collect).children('#detail').text('#' + NFTs[i].number);
+let KLAYTN_price = 0;
+let totalPrice = 0;
+let basePos = 0;
+
+fetch("https://api.bithumb.com/public/ticker/KLAY_KRW")
+.then(res => res.json())
+.then(res => {
+    KLAYTN_price = parseFloat(res.data.closing_price);
+}).then(() => {
+    NFTs.forEach(nft => {
+        totalPrice += nft.now;
+    })
+    $('#totlaPrice').text(addComma(totalPrice * totalPrice) + ' 원');
+})
+
+$(document).ready(function () {
+    $('.NFTBarTop').each(function (i, barTop) {
+        $(barTop).children('.collection').text(NFTs[i].collection);
+        $(barTop).children('#detail').text('#' + NFTs[i].number);
+    })
+
+    $('.nftSliderDiv > div').each(function (i, NFTBar) {
+        var imgStr = NFTs[i].imgName;
+
+        $(NFTBar).children('img').attr('alt', imgStr);
+
+
+        if (imgStr.indexOf('.') == -1) {
+            imgStr += '.png';
+        }
+
+        $(NFTBar).children('img').attr('src', 'images/' + imgStr);
+
+        const objectStr = ['bought', 'now', 'percent'];
+        $(NFTBar).children('div').children('div').children('#boughtPrice').each(function (j, eachPrice) {
+            if (j < 2) {
+                $(eachPrice).text(NFTs[i][objectStr[j]]);
+            } else {
+                $(eachPrice).text(((NFTs[i][objectStr[1]] / NFTs[i][objectStr[0]] - 1) * 100).toFixed(0) + '%');
+            }
+        })
     })
 });
+
+function moveTo(dir) {
+    basePos += dir;
+
+    $('.NFTBarTop').each(function (k, barTop) {
+        i = (Math.abs(k+basePos)) % NFTs.length;
+        $(barTop).children('.collection').text(NFTs[i].collection);
+        $(barTop).children('#detail').text('#' + NFTs[i].number);
+    })
+
+    $('.nftSliderDiv > div').each(function (k, NFTBar) {
+        i = (Math.abs(k+basePos)) % NFTs.length;
+
+        var imgStr = NFTs[i].imgName;
+
+        $(NFTBar).children('img').attr('alt', imgStr);
+
+
+        if (imgStr.indexOf('.') == -1) {
+            imgStr += '.png';
+        }
+
+        $(NFTBar).children('img').attr('src', 'images/' + imgStr);
+
+        const objectStr = ['bought', 'now', 'percent'];
+        totalPrice += NFTs[i].now;
+        $(NFTBar).children('div').children('div').children('#boughtPrice').each(function (j, eachPrice) {
+            if (j < 2) {
+                $(eachPrice).text(NFTs[i][objectStr[j]]);
+            } else {
+                $(eachPrice).text(((NFTs[i][objectStr[1]] / NFTs[i][objectStr[0]] - 1) * 100).toFixed(0) + '%');
+            }
+        })
+    })
+}
